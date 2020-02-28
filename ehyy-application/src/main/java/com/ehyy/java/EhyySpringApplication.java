037a7fa928951454d6c9a0b6d71ead30323a2b95
Cheney_log <1397429175@qq.com>
2020-02-28 15:52:40 +0800

    初始化提交

---
+++ ehyy-application/src/main/java/com/ehyy/java/EhyySpringApplication.java
@@ -0,0 +1,13 @@
+package com.ehyy.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhyySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhyySpringApplication.class, args);
+    }
+
+}
---
+++ ehyy-application/src/test/java/com/ehyy/java/EhyySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehyy.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhyySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
